46ff4e9ad1c6fba588425b0bc505e8d4e63b942a
Pralhad <pralhadagnihotri@gmail.com>
2024-09-25 17:01:57 +0530

    second commit

--- src/GradingSystem.java
+++ src/GradingSystem.java
@@ -7,11 +7,11 @@ public class GradingSystem {
         marks = in.nextInt();
         if(marks>90)
             System.out.println("Excellent");
-        else if((marks>80) && (marks<=90))
+        else if(marks>80)
             System.out.println("Good");
-        else if(marks>70 && marks<=80)
+        else if(marks>70)
             System.out.println("Fair");
-        else if(marks>60 && marks<=70)
+        else if(marks>60)
             System.out.println("Meets expectation");
         else
             System.out.println("Below par");
